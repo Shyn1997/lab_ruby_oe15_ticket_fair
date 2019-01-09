7e369e39df81de065790431cd4d64c0ab774781c
Nguyen Khac Thien <thiennk52@wru.vn>
2019-01-09 08:34:17 +0700

    view-first

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,53 @@
+class TicketsController < ApplicationController
+  before_action :find_ticket, only: %i(update)
+
+  def index
+    if params[:search] == :cancel.to_s
+      search_ticket
+    elsif params[:search] == :search.to_s
+      search_all_tickets
+    end
+  end
+
+  def new; end
+
+  def edit; end
+
+  def update
+    @ticket.status_cancel!
+
+    if @ticket.status_pending?
+      flash.now[:danger] = t ".cancel_fail"
+    end
+    redirect_to request.referrer
+  end
+
+  private
+
+  def find_ticket
+    @ticket = Ticket.find_by id: params[:id]
+
+    return if @ticket
+    flash[:danger] = t ".error"
+    redirect_to root_url
+  end
+
+  def search_ticket
+    return unless params[:code].present? && params[:phone_number].present?
+    @ticket = Ticket.join_customer.search_ticket(params[:code],
+      params[:phone_number]).first
+
+      if @ticket.nil?
+      flash.now[:danger] = t ".not_phone_ticket"
+    end
+  end
+
+  def search_all_tickets
+    return unless params[:phone_number].present?
+    @tickets = Ticket.join_customer.search_ticket(nil, params[:phone_number]).sorted
+
+    if @tickets.blank?
+      flash.now[:danger] = t ".not_phone"
+    end
+  end
+end
